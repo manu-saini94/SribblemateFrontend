6ae4674d701e753d13f854adacd12ae4a93e7dbf
manu-saini94 <manu.saini94@gmail.com>
2024-09-11 00:49:37 +0530

    interceptors implemented for global error handling

--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -4,13 +4,28 @@ export const useToken = (): [
   string | null,
   React.Dispatch<React.SetStateAction<string | null>>
 ] => {
-  const [token, setToken] = useState<string | null>(
-    localStorage.getItem("token")
-  );
+  // const [token, setToken] = useState<string | null>(
+  //   localStorage.getItem("token")
+  // );
+
+  const [token, setToken] = useState<string | null>(null);
+
+  const getCookie = (name: string) => {
+    const cookieName = `${name}=`;
+    const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+
+    for (const cookie of cookies) {
+      if (cookie.startsWith(cookieName)) {
+        return decodeURIComponent(cookie.substring(cookieName.length));
+      }
+    }
+    return null;
+  };
 
   useEffect(() => {
-    const storedToken = localStorage.getItem("token");
-    setToken(storedToken);
+    // const storedToken = localStorage.getItem("token");
+    const token = getCookie("accessToken");
+    setToken(token);
   }, []);
 
   return [token, setToken];
