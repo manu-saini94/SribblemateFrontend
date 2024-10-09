4c4c712bc0d7cc44b537ac5fe61c26f0cf8e694f
manu-saini94 <manu.saini94@gmail.com>
2024-10-09 11:17:21 +0530

    types for collaborator added

---
+++ src/types/userstypes/index.d.ts
@@ -0,0 +1,39 @@
+import { CreateCollaboratorType } from "@types/notetypes";
+
+export type UserDataType = {
+  expiresIn: number;
+
+  fullName: string;
+
+  email: string;
+
+  status: Utils.Status;
+
+  createdAt: Date;
+
+  updatedAt: Date;
+
+  profilePicture: string;
+};
+
+interface UserDto {
+  fullName: string;
+  email: string;
+  status: "ACTIVE" | "INACTIVE";
+  createdAt: string;
+  updatedAt: string;
+  profilePicture: string | null;
+}
+
+export type UsersInitialStateType = {
+  collaboratorExistLoading: Boolean;
+  collaboratorExistError: string;
+  usersFetchLoading: Boolean;
+  usersFetchError: string;
+};
+
+export type UsersStoreInitialStateType = UsersInitialStateType & {
+  isExist: Boolean;
+  existCollaborator: CreateCollaboratorType;
+  allUsers: UserDto[];
+};
